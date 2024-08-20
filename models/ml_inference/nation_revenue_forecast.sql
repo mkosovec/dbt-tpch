10ad917eec5d14c6d31e5ff1fe24ce775ece38f7
Matt Kosovec <matt.kosovec@gmail.com>
2024-08-20 00:35:13 +0000

    Updated with inference model

---
+++ models/ml_inference/nation_revenue_forecast.sql
@@ -0,0 +1,8 @@
+SELECT
+date_add(MAX(l.l_receiptdate),30) line_item_receipt_date,
+nation_key
+FROM {{source('tpch_samples', 'nation')}} n
+JOIN {{source('tpch_samples', 'customer')}} c on n.n_nationkey = c.c_nationkey
+JOIN {{source('tpch_samples', 'orders')}} o on c.c_custkey = o.o_custkey
+JOIN {{source('tpch_samples', 'lineitem')}} l on o.o_orderkey = l.l_orderkey
+GROUP BY nation_key
