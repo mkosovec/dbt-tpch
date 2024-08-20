bfcca7c4d1a0ce14ef1ca6da72da0a1c2eeedd59
Matt Kosovec <matt.kosovec@gmail.com>
2024-08-20 00:41:38 +0000

    Fixed to show overlap

--- models/ml_inference/nation_revenue_forecast.sql
+++ models/ml_inference/nation_revenue_forecast.sql
@@ -1,8 +1,8 @@
 SELECT
 date_add(MAX(l.l_receiptdate),30) line_item_receipt_date,
 nation_key
-FROM {{source('tpch_samples', 'nation')}} n
-JOIN {{source('tpch_samples', 'customer')}} c on n.n_nationkey = c.c_nationkey
-JOIN {{source('tpch_samples', 'orders')}} o on c.c_custkey = o.o_custkey
-JOIN {{source('tpch_samples', 'lineitem')}} l on o.o_orderkey = l.l_orderkey
+FROM {{ source('tpch_samples', 'lineitem') }} l
+LEFT JOIN {{ source('tpch_samples', 'orders') }} o on l.l_orderkey = o.o_orderkey
+LEFT JOIN {{ source('tpch_samples', 'customer') }} c on o.o_custkey = c.c_custkey
+LEFT JOIN {{ source('tpch_samples', 'nation') }} n on c.c_nationkey = n.n_nationkey
 GROUP BY nation_key
